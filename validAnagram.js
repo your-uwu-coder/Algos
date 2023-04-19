c82008eac97c0760eb19649092e792d492ecb9ab
Lynn Ventura <m_lynnventura@yahoo.com>
2023-04-18 19:12:33 -0700

    new algo challenge complete!

---
+++ validAnagram.js
@@ -0,0 +1,25 @@
+var isAnagram = function(s, t) {
+    s = (s.split("").sort())
+    t = (t.split("").sort())
+    console.log(s, t)
+
+    if (s.length !== t.length) 
+        return false;
+
+    for (let i = 0 ; i < s.length; i++) {
+        if (s[i] !== t[i]) {
+            return false
+        }
+    }
+    return true;
+};
+
+
+
+// compare if s and t are matching letters
+// sort through s and t 
+// for loop through one variable to compare to second variable
+// if both arguments are matching, return true
+// if not return false
+
+console.log(isAnagram("water", "teawr"))
