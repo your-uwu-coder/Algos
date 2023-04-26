58f75e29c7f7e1087842877ac3cc8b32137a7be5
Lynn Ventura <m_lynnventura@yahoo.com>
2023-04-26 02:07:14 -0700

    used Set object versus for loop

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -1,17 +1,17 @@
 function containsDuplicate(nums) {
-    // sort the array to find nums quicker
-    nums.sort((a,b) => a - b)
-    // for loop to check if there are two of the same numbers in the array
-    //use array.length - 1 to get the elements within the array
-    for (let i = 0 ; i < nums.length-1; i++) {
-        if (nums[i] === nums[i+1]) {
-                return true
-            }
-    }
-    return false;
-    // return true if there are two same nums
-    // false is no matching nums
+    let set = new Set(nums)
+    console.log(set)
+    console.log(nums.length)
 
+    if (set.size == nums.length) {
+        return false
+    }
+    return true
 };
 
-console.log(containsDuplicate([1,2,3,1]))
+console.log(containsDuplicate([1,4,5,7,10,1]))
+
+//utilize Set object to contain nums to remove duplicates
+// compare the new set with .size to nums.length 
+// return false if it's the same length
+//return true if not the same length
